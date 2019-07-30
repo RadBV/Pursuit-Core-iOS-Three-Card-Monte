b710722f599d1273d8b6c8cd1c3c392fae174d04
Radharani Ribas-Valongo <rad@Radharanis-MacBook-Pro.local>
2019-07-30 12:55:01 -0400

    made three cards but only one button works

--- ThreeCardMonte/ThreeCardMonte/ViewController.swift
+++ ThreeCardMonte/ThreeCardMonte/ViewController.swift
@@ -8,13 +8,54 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
-  }
-
-
+    class ViewController: UIViewController {
+        
+        @IBOutlet weak var userMessage: UILabel!
+        
+        @IBOutlet weak var button3: UIButton!
+        @IBOutlet weak var button2: UIButton!
+        @IBOutlet weak var button1: UIButton!
+        
+        func winStuff() {
+            userMessage.text = "You Win!"
+            button1.isEnabled = false
+            button2.isEnabled = false
+            button3.isEnabled = false
+        }
+        
+        func loseStuff() {
+            userMessage.text = "You Lose!"
+            button1.isEnabled = false
+            button2.isEnabled = false
+            button3.isEnabled = false
+        }
+        
+        
+        @IBAction func newGame(_ sender: UIButton) {
+            userMessage.text = "Choose a Card."
+            button1.isEnabled = true
+            button2.isEnabled = true
+            button3.isEnabled = true
+        }
+        
+        @IBAction func winningCard(_ sender: UIButton) {
+            if button3.isTouchInside == true {
+                winStuff()
+            }
+        }
+        
+        
+        @IBAction func card2(_ sender: UIButton) {
+        }
+        
+        @IBAction func card1(_ sender: UIButton) {
+            
+        }
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            // Do any additional setup after loading the view.
+        }
+        
+        
 }
 
